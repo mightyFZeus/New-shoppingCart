42d50074bef6f331572bb04dc384a54e325e5936
mightyFZeus <67365597+mightyFZeus@users.noreply.github.com>
2021-05-03 01:42:32 +0800

    Initial commit

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {Card, CardMedia, CardContent,CardActions, Typography, IconButton} from '@material-ui/core'
+import {AddShoppingCart} from '@material-ui/icons'
+import useStyles from './styles'
+const Product =({product}) =>{
+  const classes = useStyles()
+  return(
+    <div>
+        <Card className={classes.root}>
+          <CardMedia className={classes.media} img='' title={product.name} />
+          <CardContent>
+            <div className={classes.content}>
+              <Typography variant='h5' guttterBottom>
+                {product.name}
+              </Typography>
+              <Typography variant='h5' guttterBottom>
+                {product.price}
+              </Typography>
+            </div>
+            <Typography variant='h2' color='textSecondary' >
+                {product.description}
+              </Typography>
+          </CardContent>
+          <CardActions disableSpacing className={classes.cardActions}>
+            <IconButton aria-label='Add To Cart'>
+                <AddShoppingCart />
+            </IconButton>
+          </CardActions>
+        </Card>
+    </div>
+  )
+}
+
+export default Product
