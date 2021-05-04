c89ce4d32fcc4840653e6d4e7319e651fff28487
mightyFZeus <67365597+mightyFZeus@users.noreply.github.com>
2021-05-05 05:49:45 +0800

    gfhj

--- src/App.js
+++ src/App.js
@@ -3,26 +3,29 @@ import { useState, useEffect } from "react";
 import { commerce } from "./Lib/Commerce";
 import { Products, NavBar } from "./components";
 
-
 export default function App() {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
     setProducts(data);
   };
 
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
+
   useEffect(() => {
     fetchProducts();
+    fetchCart();
   }, []);
 
-  console.log(products);
+  console.log(cart);
   return (
-    
-      <>
-        <NavBar />
-        <Products products={products} />
-      </>
-    
+    <>
+      <NavBar />
+      <Products products={products} />
+    </>
   );
 }
