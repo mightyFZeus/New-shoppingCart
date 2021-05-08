87c03cc9ccbf7a39b94b012cfe765a76870a6ee3
mightyFZeus <67365597+mightyFZeus@users.noreply.github.com>
2021-05-08 07:03:27 -0700

    qwecdv f

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,78 +1,79 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import {
-  InputLabel, 
+  InputLabel,
   Select,
   MenuItem,
   Button,
   Grid,
   Typography
-} from '@material-ui/core'
-import {useForm, FormProvider} from 'react-hook-form'
-import CustomTextField from './CustomTextField'
-import {commerce} from '../../Lib/Commerce'
-const AddressForm = ({checkOutToken}) => {
-  const methods = useForm()
-  const [shippingCountries, setshippingCountries] =useState([])
-  const [shippingCountry, setshippingCountry] =useState('')
-  const [shippingSubdivisions, setshippingSubdivisions] =useState([])
-  const [shippingSubdivision, setshippingSubdivision] =useState('')
-  const [shippingOptions, setshippingOptions] =useState([])
-  const [shippingOption, setshippingOption] =useState('')
-  
-  const fetchShippingCountries  = async (checkoutTokenId) =>{
-    const {countries} = await commerce.services.localeListShippingCountries(checkoutTokenId)
-    setshippingCountries(countries)
-  }
-  
-  useEffect(() =>{
-    fetchShippingCountries(checkOutToken.id)
-  },[])
-  
-  return (
-      <>
-        <Typography variant='h6' gutterBottom>
-            Shipping
-        </Typography>
+} from "@material-ui/core";
+import { useForm, FormProvider } from "react-hook-form";
+import CustomTextField from "./CustomTextField";
+import { commerce } from "../../Lib/Commerce";
+const AddressForm = ({ checkOutToken }) => {
+  const methods = useForm();
+  const [shippingCountries, setshippingCountries] = useState([]);
+  const [shippingCountry, setshippingCountry] = useState("");
+  const [shippingSubdivisions, setshippingSubdivisions] = useState([]);
+  const [shippingSubdivision, setshippingSubdivision] = useState("");
+  const [shippingOptions, setshippingOptions] = useState([]);
+  const [shippingOption, setshippingOption] = useState("");
 
-        <FormProvider {...methods}>
-          <form onSubmit=''>
-            <Grid conatiner spacing={3}>
-              <CustomTextField required name='firstName' label='First Name' />
-              <CustomTextField required name='lastName' label='Last Name' />
-              <CustomTextField required name='address1' label='Address' />
-              <CustomTextField required name='email' label='Email' />
-              <CustomTextField required name='city' label='City' />
-              <CustomTextField required name='zip' label='ZIP / Postal code' />
-              <Grid item xs={12}  sm={6}>
-                <InputLabel>Shipping Country</InputLabel>
-                <Select value='' fullWidth onnChange=''>
-                  <MenuItem key='' value=''>
-                    select me
-                  </MenuItem>
-                </Select>
-              </Grid>
-              <Grid item xs={12}  sm={6}>
-                <InputLabel>Shipping SubDivision</InputLabel>
-                <Select value='' fullWidth onnChange=''>
-                  <MenuItem key='' value=''>
-                    select me
-                  </MenuItem>
-                </Select>
-              </Grid>
-              <Grid item xs={12}  sm={6}>
-                <InputLabel>Shipping Options</InputLabel>
-                <Select value='' fullWidth onnChange=''>
-                  <MenuItem key='' value=''>
-                    select me
-                  </MenuItem>
-                </Select>
-              </Grid>
+  const fetchShippingCountries = async (checkoutTokenId) => {
+    const { countries } = await commerce.services.localeListShippingCountries(
+      checkoutTokenId
+    );
+    setshippingCountries(countries);
+  };
+
+  useEffect(() => {
+    fetchShippingCountries(checkOutToken.id);
+  }, []);
+
+  return (
+    <>
+      <Typography variant="h6" gutterBottom>
+        Shipping
+      </Typography>
+
+      <FormProvider {...methods}>
+        <form onSubmit="">
+          <Grid container spacing={3}>
+            <CustomTextField required name="firstName" label="First Name" />
+            <CustomTextField required name="lastName" label="Last Name" />
+            <CustomTextField required name="address1" label="Address" />
+            <CustomTextField required name="email" label="Email" />
+            <CustomTextField required name="city" label="City" />
+            <CustomTextField required name="zip" label="ZIP / Postal code" />
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Country</InputLabel>
+              <Select value="" fullWidth onChange="">
+                <MenuItem key="" value="">
+                  select me
+                </MenuItem>
+              </Select>
             </Grid>
-          </form>
-        </FormProvider>
-      </>
-     
-  )
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping SubDivision</InputLabel>
+              <Select value="" fullWidth onnChange="">
+                <MenuItem key="" value="">
+                  select me
+                </MenuItem>
+              </Select>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Options</InputLabel>
+              <Select value="" fullWidth onnChange="">
+                <MenuItem key="" value="">
+                  select me
+                </MenuItem>
+              </Select>
+            </Grid>
+          </Grid>
+        </form>
+      </FormProvider>
+    </>
+  );
 };
 
 export default AddressForm;
